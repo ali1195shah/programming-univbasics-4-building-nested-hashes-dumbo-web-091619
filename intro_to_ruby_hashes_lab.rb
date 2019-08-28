81639866daf0212f12fd45557dc39e3f208e1cd0
Ali Shah <ali1195shah@gmail.com>
2019-08-28 01:24:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,6 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads => {:pieces => 4}}
 end
 
 def monopoly_with_third_tier
@@ -17,14 +16,6 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads => {:rent_in_dollars => {
-    :one_piece_owned => 25, 
-    :two_piece_owned => 50, 
-    :three_piece_owned => 100, 
-    :four_piece_owned => 200},
-    :name => {
-      
-    }}}
 end
 
 def monopoly_with_fourth_tier
